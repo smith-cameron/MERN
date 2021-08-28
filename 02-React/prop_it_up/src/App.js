4609d868e26d0ff8504c25c82627d4e42ec555a6
Cameron Smith <theauspiciouscammy@gmail.com>
2021-08-28 13:45:11 -0700

    complete

---
+++ 02-React/prop_it_up/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React from 'react';
+import PersonCard from './components/PropsCo';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard
+        lastName={"Robinson"}
+        firstName={"Joel"}
+        age={53}
+        hair={"dark brown"}
+      />
+      <PersonCard
+        lastName={"Robot"}
+        firstName={"Crow T."}
+        age={31}
+        hair={"none"}
+      />
+      <PersonCard lastName={"Servo"} firstName={"Tom"} age={31} hair={"none"} />
+      <PersonCard
+        lastName={"Frank"}
+        firstName={"TV's"}
+        age={55}
+        hair={"white"}
+      />
+    </div>
+  );
+}
+
+export default App;
