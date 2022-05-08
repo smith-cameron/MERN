e5600dbe22954952ccc283b8a18df59ea6175c96
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-05-08 08:08:13 -0700

    update

---
+++ theBig_O.js
@@ -0,0 +1,49 @@
+//-1-Rewrite isPrime() to calculate primes faster
+//I don't need to search past the square root of the number I am checking to see if it is evenly divisible by smaller numbers.
+Number.prototype.isPrime = function() {
+        for( let i=2; i<this; i++ ) {
+            if( this % i === 0 ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+const { performance } = require('perf_hooks');
+const start = performance.now();
+let primeCount = 0;
+let num = 2; // for math reasons, 1 is considered prime
+while( primeCount < 1e4 ) {
+    if( num.isPrime() ) {
+        primeCount++;
+    }
+    num++;
+}
+console.log(`The 10,000th prime number is ${num-1}`);
+console.log(`This took ${performance.now() - start} milliseconds to run`);
+
+
+//-2-Which implementation of Fibonacci should be faster?
+// recursive
+function rFib( n ) {
+    if ( n < 2 ) {
+        return n;
+    }
+    return rFib( n-1 ) + rFib( n-2 );
+}
+rFib(20);
+// iterative
+function iFib( n ) {
+    const vals = [ 0, 1 ];
+    while(vals.length-1 < n) {
+        let len = vals.length;
+        vals.push( vals[len-1] + vals[len-2] );
+    }
+    return vals[n];
+}
+iFib(20);
+
+//-3-Can we reverse a string more efficiently?
+//Use one loop
+const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+const reversed1 = story.split("").reverse().join("");
