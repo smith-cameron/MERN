46efe30609ff18d0d640275cdc4dde5acba8a5bc
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-06-15 15:42:41 -0700

    update

---
+++ 00Skeleton00/client/src/components/Landing.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react';
+const Landing = () => {        
+  return (
+    <div>
+      <h1>Houston, we have liftoff.</h1>
+    </div>
+    )
+  }
+  export default Landing;
